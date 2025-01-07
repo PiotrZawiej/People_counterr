b25d304554bcd6905747261f18d50b9d46196d99
unknown <piotr.zawieja0x@gmail.com>
2025-01-07 21:46:09 +0100

    added photo_detector function

--- people_detector.py
+++ people_detector.py
@@ -1,22 +1,20 @@
 import cv2
-image = cv2.imread("picture\humans.jpg",0)
-winSize = (64,64)
-blockSize = (16,16)
-blockStride = (8,8)
-cellSize = (8,8)
-nbins = 9
-derivAperture = 1
-winSigma = 4.
-histogramNormType = 0
-L2HysThreshold = 2.0000000000000001e-01
-gammaCorrection = 0
-nlevels = 64
-hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
-                        histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
-#compute(img[, winStride[, padding[, locations]]]) -> descriptors
-winStride = (8,8)
-padding = (8,8)
-locations = ((10,20),)
-hist = hog.compute(image,winStride,padding,locations)
-hog = cv2.HOGDescriptor()
-hog.save("hog.xml")
+
+def photo_detector(photo_dir):
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    image = cv2.imread(photo_dir)
+
+    (rects, weights) = hog.detectMultiScale(image, winStride=(7, 7), padding=(8, 8), scale=1.05)
+
+    num_humans = len(rects)
+    print(f"Number of humans detected: {num_humans}")
+
+    for (x, y, w, h) in rects:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imwrite("output.jpg", image)
+
+photo_detector("picture\humans.jpg")
+
