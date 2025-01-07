ff8bd19a093dec66170b8bebed713b600db3c127
unknown <piotr.zawieja0x@gmail.com>
2025-01-07 22:15:42 +0100

    added get_photo function

--- people_detector.py
+++ people_detector.py
@@ -1,6 +1,6 @@
 import cv2
 
-def photo_detector(photo_dir):
+def people_detector(photo_dir):
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
@@ -9,12 +9,10 @@ def photo_detector(photo_dir):
     (rects, weights) = hog.detectMultiScale(image, winStride=(7, 7), padding=(8, 8), scale=1.05)
 
     num_humans = len(rects)
-    print(f"Number of humans detected: {num_humans}")
 
     for (x, y, w, h) in rects:
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imwrite("output.jpg", image)
 
-photo_detector("picture\humans.jpg")
-
+    return num_humans
