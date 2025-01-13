ff4fb0e3b510747f6b5aa03bcddfee127e615c9c
unknown <piotr.zawieja0x@gmail.com>
2025-01-13 19:16:46 +0100

    id not working

--- add_task.py
+++ add_task.py
@@ -1,10 +1,8 @@
 import pika
 import sys
-from datetime import datetime
-from uuid import uuid4
 import json
 
-eventid = datetime.now().strftime('%Y%m-%d%H-%M%S-') + str(uuid4())
+
 
 def get_connection():
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -16,13 +14,18 @@ def get_connection():
 def declare_queue(channel):
     channel.queue_declare(queue='people_detector', durable=True)
 
-def add_task_to_queue(url : str):
+
+
+def add_task_to_queue(url : str, eventID):
+
     connection = get_connection()
     channel = connection.channel()
 
     declare_queue(channel)
 
-    message = json.dumps({"url": url})
+    message = json.dumps({"url": url, "eventID": eventID})
+    
+    print(f"Sending message: {message}")
     channel.basic_publish(exchange='',
                         routing_key='people_detector',
                         body= message,
@@ -31,7 +34,5 @@ def add_task_to_queue(url : str):
                         ))
 
     connection.close()
-    return {"message": "Task added to queue"}
+    return {"message": "Task added to queue", "eventid": eventID}
 
-url = "https://t4.ftcdn.net/jpg/02/87/41/47/360_F_287414734_OKNLmIbSObUKIELfwEK6eu52cdRV5HAK.jpg"
-add_task_to_queue(url)
--- routers.py
+++ routers.py
@@ -1,16 +1,21 @@
 from fastapi import HTTPException, APIRouter
 from add_task import add_task_to_queue
-from receive import start_worker
-
 router = APIRouter()
+from uuid import uuid4
+from datetime import datetime
+
+
+
+def generate_eventid():
+    return datetime.now().strftime('%Y%m-%d%H-%M%S-') + str(uuid4())
 
 @router.get("/add_task")
 async def get_photo():
     url = "https://t4.ftcdn.net/jpg/02/87/41/47/360_F_287414734_OKNLmIbSObUKIELfwEK6eu52cdRV5HAK.jpg"
-    add_task_to_queue(url)
+    id = generate_eventid()
+
+    add_task_to_queue(url, id)
 
     return {"task": "added"}
 
-router.get("/receive_task")
-async def consume_task():
-    start_worker()
+
